e9504f1ded80b39fa83f77fd0a17f6ac00170836
wowba <leeyuwk54@gmail.com>
2022-05-10 20:01:37 +0900

    프론트엔드 기본페이지 띄우기

---
+++ frontend/app.js
@@ -0,0 +1,15 @@
+// npm으로 express 설치 후 사용 가능
+const express = require('express')
+
+const app = express();
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile("C:/Users/user/Desktop/2nd_week_board/frontend/public/blog-list.html")
+});
+
+// express는 절대경로를 사용하기에 상대경로를 사용하는 파일들은 static folder를 사용해 넣어준다.
+app.use(express.static('public'));
+
+app.listen(PORT)
