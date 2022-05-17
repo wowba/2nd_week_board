d0ed8b66281dfd24eff07c495eb973355b26632e
wowba <leeyuwk54@gmail.com>
2022-05-17 10:54:54 +0900

    컨트롤러 스웨거 내용추가

--- backend/board/src/main/java/com/sph/board/controller/BoardController.java
+++ backend/board/src/main/java/com/sph/board/controller/BoardController.java
@@ -23,20 +23,27 @@ import com.sph.board.dto.BoardResponseDto;
 import com.sph.board.message.RestResponseMessage;
 import com.sph.board.service.BoardService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 
+@Api(tags = {"게시판 관련 컨트롤러"})
 @RestController
 @RequiredArgsConstructor
 public class BoardController {
 
 	private final BoardService boardService;
 
+	@ApiOperation(value = "게시판 생성하기")
 	@PostMapping("/api/board")
 	public ResponseEntity<RestResponseMessage> createBoard(@Valid @RequestBody BoardRequestDto boardRequestDto) {
 		boardService.createBoard(boardRequestDto);
 		return new ResponseEntity<>(new RestResponseMessage<>(true, "게시판 생성하기", null), HttpStatus.OK);
 	}
 
+	@ApiOperation(value = "게시판 리스트 가져오기")
+	@ApiImplicitParam(name = "page", value = "페이지 값", required = true, dataType = "int")
 	@GetMapping("/api/board")
 	public ResponseEntity<RestResponseMessage> getAllBoard(@RequestParam int page) {
 		PageRequest pageRequest = PageRequest.of(page - 1, 15, Sort.by(Sort.Direction.DESC, "createdAt"));
@@ -44,12 +51,16 @@ public class BoardController {
 		return new ResponseEntity<>(new RestResponseMessage<>(true, "게시판 리스트 가져오기", data), HttpStatus.OK);
 	}
 
+	@ApiOperation(value = "게시판 상세내용 가져오기")
+	@ApiImplicitParam(name = "id", value = "게시판 id", required = true, dataType = "int")
 	@GetMapping("/api/board/{id}")
 	public ResponseEntity<RestResponseMessage> getBoard(@PathVariable long id) {
 		BoardResponseDto boardResponseDto = boardService.getBoard(id);
 		return new ResponseEntity<>(new RestResponseMessage<>(true, "게시판 상세내용 가져오기", boardResponseDto), HttpStatus.OK);
 	}
 
+	@ApiOperation(value = "게시판 수정하기")
+	@ApiImplicitParam(name = "id", value = "게시판 id", required = true, dataType = "int")
 	@PatchMapping("/api/board/{id}")
 	public ResponseEntity<RestResponseMessage> editBoard(@PathVariable long id,
 		@Valid @RequestBody BoardRequestDto boardRequestDto) {
@@ -57,12 +68,14 @@ public class BoardController {
 		return new ResponseEntity<>(new RestResponseMessage(true, "게시판 수정하기", null), HttpStatus.OK);
 	}
 
+	@ApiOperation(value = "게시판 삭제하기")
 	@DeleteMapping("/api/board")
 	public ResponseEntity<RestResponseMessage> deleteBoard(@RequestBody Map<String, String> param) {
 		boardService.deleteBoard(param);
 		return new ResponseEntity<>(new RestResponseMessage(true, "게시판 삭제하기", null), HttpStatus.OK);
 	}
 
+	@ApiOperation(value = "더미 게시판 생성하기")
 	@PostMapping("/api/dummy")
 	public ResponseEntity<RestResponseMessage> createDummy() throws InterruptedException {
 		boardService.createDummy();
